ced5a7ca33c6910e6f8b819eab462d6f3010eff5
heimao-sudo <2329982264@qq.com>
2025-12-30 23:54:23 +0800

    nano-course.md

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import type {
+=import type {
         AnnouncementConfig,
         CommentConfig,
         ExpressiveCodeConfig,
@@ -308,39 +308,39 @@ export const navBarConfig: NavBarConfig = {
                                 },
                         ],
                 },
-                {
-                        name: "Others",
-                        url: "#",
-                        icon: "material-symbols:more-horiz",
-                        children: [
-                                {
-                                        name: "Projects",
-                                        url: "/projects/",
-                                        icon: "material-symbols:work",
-                                },
-                                {
-                                        name: "Skills",
-                                        url: "/skills/",
-                                        icon: "material-symbols:psychology",
-                                },
-                                {
-                                        name: "Timeline",
-                                        url: "/timeline/",
-                                        icon: "material-symbols:timeline",
-                                },
-                        ],
-                },
+//                {
+//                        name: "Others",
+//                        url: "#",
+//                        icon: "material-symbols:more-horiz",
+//                        children: [
+//                                {
+//                                        name: "Projects",
+//                                        url: "/projects/",
+//                                        icon: "material-symbols:work",
+//                                },
+//                                {
+//                                        name: "Skills",
+//                                        url: "/skills/",
+//                                        icon: "material-symbols:psychology",
+//                                },
+//                                {
+//                                        name: "Timeline",
+//                                        url: "/timeline/",
+//                                        icon: "material-symbols:timeline",
+//                                },
+//                        ],
+//                },
         ],
 };
-
-export const profileConfig: ProfileConfig = {
-        avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-        name: "Matsuzaka Yuki",
-        bio: "The world is big, you have to go and see",
-        typewriter: {
-                enable: true, // 启用个人简介打字机效果
-                speed: 80, // 打字速度（毫秒）
-        },
+//
+//export const profileConfig: ProfileConfig = {
+//        avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+//        name: "Matsuzaka Yuki",
+//        bio: "The world is big, you have to go and see",
+//        typewriter: {
+//                enable: true, // 启用个人简介打字机效果
+//                speed: 80, // 打字速度（毫秒）
+//        },
         links: [
                 {
                         name: "Bilibli",
