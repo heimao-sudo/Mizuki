21b9646b5c948639c2128c0f97dbd28ab428038d
heimao-sudo <2329982264@qq.com>
2025-12-31 00:28:16 +0800

    nano-course.md

--- src/config.ts
+++ src/config.ts
@@ -307,7 +307,7 @@ export const navBarConfig: NavBarConfig = {
                                         icon: "material-symbols:group",
                                 },
                         ],
-                }
+                },
 //                {
 //                        name: "Others",
 //                        url: "#",
@@ -332,15 +332,15 @@ export const navBarConfig: NavBarConfig = {
 //                },
         ],
 };
-//
-//export const profileConfig: ProfileConfig = {
-//        avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-//        name: "Matsuzaka Yuki",
-//        bio: "The world is big, you have to go and see",
-//        typewriter: {
-//                enable: true, // 启用个人简介打字机效果
-//                speed: 80, // 打字速度（毫秒）
-//        },
+
+export const profileConfig: ProfileConfig = {
+        avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+        name: "Matsuzaka Yuki",
+        bio: "The world is big, you have to go and see",
+        typewriter: {
+                enable: true, // 启用个人简介打字机效果
+                speed: 80, // 打字速度（毫秒）
+        },
         links: [
                 {
                         name: "Bilibli",
